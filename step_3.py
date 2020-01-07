7e959df2b64e90c4839230a15a54f58e8235eaa4
Dyakov-QA <dyakov.serhey@gmail.com>
2020-01-07 19:29:50 +0200

    finally working test

--- step_3.py
+++ step_3.py
@@ -13,18 +13,24 @@ def browser():
     browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
-    time.sleep(10)
     browser.quit()
 
 
-@pytest.mark.parametrize('parameter', ["236895", "236896"])
+@pytest.mark.parametrize('parameter', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
 def test_guest_should_see_login_link(browser, parameter):
     link = f"https://stepik.org/lesson/{parameter}/step/1/"
     browser.get(link)
     answer = str(math.log(int(time.time())))
-    print(answer)
-    # time.sleep(10)
     text_area = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, ".textarea"))
     )
     text_area.send_keys(answer)
+
+    submit = browser.find_element_by_css_selector(".submit-submission")
+    submit.click()
+
+    feedback = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".smart-hints__hint"))
+    )
+    print(feedback.text)
+    assert feedback.text == "Correct!", "Wrong text"
